e8a6afd6c086ad584a29515c4db4651c5fbeae22
Camila <mattosmia@gmail.com>
2018-02-01 17:25:48 +0000

    Pointing basename to subdir in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter basename="/cluedo-gen">
 		<App />
 	</BrowserRouter>
 	, document.getElementById('root')
